ac2978a1acd68f921c137a51e309b2d6f4fd75e6
Jay <jylee1044@gmail.com>
2023-05-28 23:06:33 +0900

    Changed:v2

--- settings.py
+++ settings.py
@@ -1,37 +1,37 @@
 DB_SETTINGS = {
     'source_db_localhost_rdb': {
-        'host' : "127.0.0.1",
+        'host' : "",
         'port' : "5432",
         'user' : "postgres",
-        'password' : "qwe123",
-        'database' : "dvdlental",
+        'password' : "",
+        'database' : "",
         'location' : "localhost_source",
         'engine' : "postgre"
     },
     'source_db_localhost_ddb' : {
-        'host' : "127.0.0.1",
+        'host' : "",
         'port' : 27017,
         'user' : "root",
-        'password' : "qwe123",
-        'database' : "yes24",
+        'password' : "",
+        'database' : "",
         'location' : "localhost_source",
         'engine' : "mongodb"
     },
     'target_db_localhost_rdb' : {
-        'host' : "127.0.0.1",
+        'host' : "",
         'port' : "5432",
         'user' : "postgres",
-        'password' : "qwe123",
-        'database' : "temp0",
+        'password' : "",
+        'database' : "",
         'location' : "localhost_target",
         'engine' : "postgre"
     },
     'target_db_localhost_ddb' : {
-        'host' : "127.0.0.1",
+        'host' : "",
         'port' : 27017,
-        'user' : "root",
-        'password' : "qwe123",
-        'database' : "temp0",
+        'user' : "",
+        'password' : "",
+        'database' : "",
         'location' : "localhost_target",
         'engine' : "mongodb"
     },
